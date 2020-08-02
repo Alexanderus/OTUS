aa981a1d0f67acbda0c9cc7a39295524f1710b75
Aleksandr Ostapenko <avostapenko@yandex.ru>
2020-08-02 09:33:51 +0300

    harry potter square

---
+++ src/main/java/magicSquare/Main.java
@@ -0,0 +1,13 @@
+package magicSquare;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int x=0; x<25; x++) {
+            for (int y=0; y<25; y++) {
+                System.out.print(((x + y) > 19) && ((x + y) < 29) ? "* " : ". ");
+
+            }
+            System.out.println("");
+        }
+    }
+}
